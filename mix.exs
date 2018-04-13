2070816b99d0695caf04e43846dddcb3c1633809
Renê Schneider <rene.schneider@oqvestir.com.br>
2018-04-13 15:30:46 -0300

    Set default config values to ""

--- mix.exs
+++ mix.exs
@@ -13,20 +13,17 @@ defmodule NewrelicInsights.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      env: [account_id: "", api_key: ""]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:tesla, "~> 1.0.0-beta.1"},
       {:jason, ">= 1.0.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
