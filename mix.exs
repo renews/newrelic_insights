c0589ef48516629fc909737c22c6505ee814ef88
Renê Schneider <rene.schneider@oqvestir.com.br>
2018-04-13 13:41:57 -0300

    Add Tesla as dependency.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule NewrelicInsights.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "This fly into the sky",
+      package: package()
     ]
   end
 
@@ -21,8 +23,18 @@ defmodule NewrelicInsights.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:tesla, "~> 1.0.0-beta.1"},
+      {:jason, ">= 1.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [" Renê Schneider, Thales Flores "],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/renews/newrelic_insights"}
     ]
   end
 end
