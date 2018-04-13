264d50628a52aa04e86d783d6f72899fba082891
Renê Schneider <rene.schneider@oqvestir.com.br>
2018-04-13 17:49:16 -0300

    Create configuration module.

---
+++ lib/newrelic_insights/configuration.ex
@@ -0,0 +1,18 @@
+defmodule NewrelicInsights.Configuration do
+  def account_id do
+    Application.fetch_env!(:newrelic_insights, :account_id)
+    |> check_configuration
+  end
+
+  def api_key do
+    Application.fetch_env!(:newrelic_insights, :api_key)
+    |> check_configuration
+  end
+
+  defp check_configuration(config) do
+    case config do
+      false -> {:error, "[newrelic_insights] Configuration not correctly set."}
+      _ -> {:ok, config}
+    end
+  end
+end
